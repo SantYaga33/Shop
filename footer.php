b07be438beb305c200b155b2f98cb48e10a85ce3
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-16 18:44:48 +0400

    верста 2 стр +3

---
+++ footer.php
@@ -0,0 +1,89 @@
+<section class="about">
+		<div class="container">
+			<div class="about__block">
+				<div class="about__item">
+					<div class="about__item-icon">
+						<img src="img/about/about-icon1.png" alt="иконка">
+					</div>
+					<span class="about__item-text">15 лет в оффлайне и <br>5 лет в интернете</span>
+					</div>
+					<div class="about__item">
+					<div class="about__item-icon">
+						<img src="img/about/about-icon2.png" alt="иконка">
+					</div>
+					<span class="about__item-text">2 недели на обмен и возврат товара</span>
+					</div>
+					<div class="about__item">
+					<div class="about__item-icon">
+						<img src="img/about/about-icon3.png" alt="иконка">
+					</div>
+					<span class="about__item-text">Товар проверяем перед отправкой</span>
+					</div>
+					<div class="about__item">
+					<div class="about__item-icon">
+						<img src="img/about/about-icon4.png" alt="иконка">
+					</div>
+					<span class="about__item-text">Накопительные скидки, подарки и акции!</span>
+					</div>
+					<div class="about__item">
+					<div class="about__item-icon">
+						<img src="img/about/about-icon5.png" alt="иконка">
+					</div>
+					<span class="about__item-text">Бесплатная доставка от 1000 грн по Украине</span>
+					</div>
+				</div>
+			</div>
+	</section>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="footer__block">
+				<div class="footer__block-item">
+					<div class="footer__title">Свяжитесь с нами</div>
+					<a href="tel:+79871880808" class="footer__link-call">+7 987 188 08 08</a>
+					<button class="button footer__button">Перезвоните мне</button>
+					<a href="#" class="footer__link_big">Блог</a>
+					<a href="#" class="footer__link_big">Новости блога</a>
+					<a href="#" class="footer__link_big">Отзывы</a>
+				</div>
+				<div class="footer__block-item">
+					<div class="footer__title">Помощь</div>
+					<a href="#" class="footer__link">Условия доставки</a>
+					<a href="#" class="footer__link">Условия оплаты</a>
+					<a href="#" class="footer__link">Условия возврата</a>
+					<a href="#" class="footer__link">Гарантия</a>
+					<a href="#" class="footer__link">Как применить промокод</a>
+					<a href="#" class="footer__link">Вернуть товар. Инструкция</a>
+					<a href="#" class="footer__link">Отследить посылку</a>
+				</div>
+				<div class="footer__block-item">
+					<div class="footer__title">Мой кабинет</div>
+					<a href="#" class="footer__link">Личный Кабинет</a>
+					<a href="#" class="footer__link">История заказов</a>
+					<a href="#" class="footer__link">Избранное</a>
+					<div class="footer__title delivery">Быстрая доставка</div>
+					<a href="#" class="footer__logo-delivery">
+						<img src="img/footer/nova-poshta-logo.png" alt="логотип">          
+					</a>
+				</div>
+				<div class="footer__block-item">
+					<div class="footer__title">Добавляйся к нам</div>
+					<div class="footer__block-icons">
+					<a class="footer__icon">
+						<img src="img/footer/instagram-symbol.svg" alt="иконка">
+					</a>
+					<a class="footer__icon">
+						<img src="img/footer/instagram-symbol.svg" alt="иконка">
+					</a>
+					<a class="footer__icon">
+						<img src="img/footer/instagram-symbol.svg" class="svg" alt="иконка">
+					</a>
+					</div>
+				</div>
+			</div>
+			<div class="footer__copyright">
+				<img src="img/footer/copyright-symbol.svg" class="footer__copyright-img" alt="иконка">
+				<span class="footer__copyright-text">www.nadejda.com 2008-2017. Все права защищены.</span>
+			</div>
+		</div>
+	</footer>
---
+++ header.php
@@ -0,0 +1,39 @@
+<header class="header">
+		<div class="header-top">
+			<div class="container">
+				<div class="header-wrap">
+					<div class="header-contacts">
+						<div class="header-contacts__phone">
+							<a class="header-phone" href="tel:9871880808">(987) 188 08 08</a>
+							<a class="header-phone" href="tel:9683550505">(968) 355 05 05</a>
+						</div>
+						<button class="button button-header">Перезвоните мне</button>
+					</div>
+					<div class="header-logo">
+						<img src="img/header/header-logo.png" alt="логотип">
+					</div>
+					<div class="header-icons">
+						<a href="#" class="header-icons__item">
+							<div class="header-icons__item-img">
+								<img src="img/header/icon-dress.svg" alt="иконка">
+							</div>
+							<span class="header-icons__item-text">Вход</span>
+						</a>
+						<a href="#" class="header-icons__item">
+							<div class="header-icons__item-img">
+								<img src="img/header/icon-heart.svg" alt="иконка">
+							</div>
+							<span class="header-icons__item-text">Избранное</span>
+						</a>
+						<a href="#" class="header-icons__item">
+							<div class="header-icons__item-img">
+								<img src="img/header/icon-card.svg" alt="иконка">
+							</div>
+							<span class="header-icons__item-text">Корзина</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<nav class="nav"></nav>
+	</header>
