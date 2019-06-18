dd40b8e735fe9590f566b525b7686aaf1fea0ee4
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-19 00:02:02 +0400

    +

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-<section class="about">
+
+	<section class="about">
 		<div class="container">
 			<div class="about__block">
 				<div class="about__item">
@@ -38,15 +39,15 @@
 	<footer class="footer">
 		<div class="container">
 			<div class="footer__block">
-				<div class="footer__block-item">
+				<div class="footer__block-item footer__block-item1">
 					<div class="footer__title">Свяжитесь с нами</div>
 					<a href="tel:+79871880808" class="footer__link-call">+7 987 188 08 08</a>
-					<button class="button footer__button">Перезвоните мне</button>
+					<button class="button footer__button">Перезвоните&nbsp;мне</button>
 					<a href="#" class="footer__link_big">Блог</a>
 					<a href="#" class="footer__link_big">Новости блога</a>
 					<a href="#" class="footer__link_big">Отзывы</a>
 				</div>
-				<div class="footer__block-item">
+				<div class="footer__block-item footer__block-item2">
 					<div class="footer__title">Помощь</div>
 					<a href="#" class="footer__link">Условия доставки</a>
 					<a href="#" class="footer__link">Условия оплаты</a>
@@ -56,11 +57,11 @@
 					<a href="#" class="footer__link">Вернуть товар. Инструкция</a>
 					<a href="#" class="footer__link">Отследить посылку</a>
 				</div>
-				<div class="footer__block-item">
+				<div class="footer__block-item footer__block-item3">
 					<div class="footer__title">Мой кабинет</div>
 					<a href="#" class="footer__link">Личный Кабинет</a>
 					<a href="#" class="footer__link">История заказов</a>
-					<a href="#" class="footer__link">Избранное</a>
+					<a href="#" class="footer__link footer__link_fix">Избранное</a>
 					<div class="footer__title delivery">Быстрая доставка</div>
 					<a href="#" class="footer__logo-delivery">
 						<img src="img/footer/nova-poshta-logo.png" alt="логотип">          
@@ -81,6 +82,8 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="footer__container">
 			<div class="footer__copyright">
 				<img src="img/footer/copyright-symbol.svg" class="footer__copyright-img" alt="иконка">
 				<span class="footer__copyright-text">www.nadejda.com 2008-2017. Все права защищены.</span>
@@ -88,8 +91,7 @@
 		</div>
 	</footer>
 
-
-		<!-- == кнопка на вверх == -->
+	<!-- == кнопка на вверх == -->
 	<div class="arrowup">
 		<svg  xmlns="http://www.w3.org/2000/svg" width="21" height="12" viewBox="0 0 21 12"><g><g><path class="arrowup" fill="#fff" d="M.197 9.889L9.389.697a.998.998 0 0 1 .793-.29.983.983 0 0 1 1.494-.122l9.038 9.039a.983.983 0 0 1-1.39 1.39l-8.562-8.562-9.151 9.151A1 1 0 1 1 .197 9.89z"/></g></g></svg>
 	</div>
--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<header class="header">
+	<header class="header">
 		<div class="header-top">
 			<div class="container">
 				<div class="header-wrap">
@@ -9,31 +9,235 @@
 						</div>
 						<button class="button button-header">Перезвоните мне</button>
 					</div>
-					<div class="header-logo">
-						<img src="img/header/header-logo.png" alt="логотип">
-					</div>
-					<div class="header-icons">
-						<a href="#" class="header-icons__item">
-							<div class="header-icons__item-img">
-								<img src="img/header/icon-dress.svg" alt="иконка">
+						<div class="header-logo__wrap">
+							<div class="header-logo">
+								<img src="img/header/header-logo.png" alt="логотип">
 							</div>
-							<span class="header-icons__item-text">Вход</span>
-						</a>
-						<a href="#" class="header-icons__item">
-							<div class="header-icons__item-img">
-								<img src="img/header/icon-heart.svg" alt="иконка">
+						</div>
+						<div class="header-icons__wrap">
+							<div class="header-icons">
+								<a href="#" class="header-icons__item">
+									<div class="header-icons__item-img">
+										<img src="img/header/icon-dress.svg" alt="иконка">
+									</div>
+									<span class="header-icons__item-text">Вход</span>
+								</a>
+								<a href="#" class="header-icons__item">
+									<div class="header-icons__item-img">
+										<img src="img/header/icon-heart.svg" alt="иконка">
+									</div>
+									<span class="header-icons__item-text">Избранное</span>
+								</a>
+								<a href="#" class="header-icons__item">
+									<div class="header-icons__item-img">
+										<img src="img/header/shopping-cart (1).svg" alt="иконка">
+									</div>
+									<span class="header-icons__item-text">Корзина</span>
+								</a>
 							</div>
-							<span class="header-icons__item-text">Избранное</span>
-						</a>
-						<a href="#" class="header-icons__item">
-							<div class="header-icons__item-img">
-								<img src="img/header/icon-card.svg" alt="иконка">
-							</div>
-							<span class="header-icons__item-text">Корзина</span>
-						</a>
-					</div>
+						</div>	
 				</div>
 			</div>
 		</div>
-		<nav class="nav"></nav>
+		<nav class="nav">
+			<div class="container">
+				<div class="nav__wrap">
+				  <div class="nav__select-groop">
+				  	<div class="nav__burger-wrap">
+							<div class="nav__burger"></div>
+						</div>
+				  	<ul>
+				  		<li class="nav__select"><a href="#" class="nav__select_blue">Одежда
+				  			<div class="nav__select-icon">
+				  			<img src="img/header/arrow-list.png" alt="стрелка">
+				  			</div></a>
+				  			<ul>
+
+			  					<li class="nav__select-sub"><a href="#">Одежда и обувь
+			  						<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a> 
+			  						<ul>
+
+			  							<li class="nav__select-sub2"><a href="#">Малышам 0-2 года
+			  								<div class="nav__select-arrow">
+						  						<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  					</div></a>
+						  				</li>
+			  							<li class="nav__select-sub2"><a href="#">Девочкам 2-14 лет
+			  								<div class="nav__select-arrow">
+						  						<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  					</div></a>
+						  				</li>
+			  							<li class="nav__select-sub2"><a href="#">Мальчикам 2-14 лет
+			  								<div class="nav__select-arrow">
+						  						<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  					</div></a>
+		  									<ul>
+
+		  										<li><a href="#">Верхняя одежда</a></li>
+		  										<li><a href="#">Джинсы</a></li>
+		  										<li><a href="#">Костюмы</a></li>
+		  										<li><a href="#">Кофты</a></li>
+		  										<li><a href="#">Куртки</a></li>
+		  										<li><a href="#">Носки</a></li>
+		  										<li><a href="#">Футболки</a></li>
+		  										<li><a href="#">Штаны</a></li>
+		  										<li><a href="#">Все категории</a></li>
+		  									</ul> 	
+			  							</li>
+
+			  							<li class="nav__select-sub2"><a href="#">Новинки
+			  								<div class="nav__select-arrow">
+						  						<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  					</div></a>
+						  				</li>
+			  						</ul>
+			  					</li>
+
+				  					<li class="nav__select-sub"><a href="#">Питание и кормление<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+				  					<li class="nav__select-sub"><a href="#">Гигиена и уход за ребенком<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+				  					<li class="nav__select-sub"><a href="#">Детская комната<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+				  					<li class="nav__select-sub"><a href="#">Игрушки<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+				  					<li class="nav__select-sub"><a href="#">Бренды<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+				  					<li class="nav__select-sub"><a href="#">Распродажа %<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+						  		</li>
+				  				</li>
+				  			</ul>
+				  		</li>
+
+							<li class="nav__select"><a href="#" class="nav__select_blue">Текстиль<div class="nav__select-icon">
+				  			<img src="img/header/arrow-list.png" alt="стрелка">
+				  			</div></a>
+								<ul>
+
+									<li class="nav__select-sub"><a href="#">Одежда<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+										<ul>
+											<li class="nav__select-sub2"><a href="#">Обувь<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Куртки<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Брюки<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+										</ul>
+									</li>
+									<li class="nav__select-sub"><a href="#">Электроника<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+										<ul>
+											<li class="nav__select-sub2"><a href="#">Камеры<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Компьютеры<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Телефоны<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+												<ul>
+													<li><a href="#">Samsung</a></li>
+													<li><a href="#">Flf</a></li>
+													<li><a href="#">Apple</a></li>
+												</ul>
+											</li>
+										</ul> 
+									</li> 
+
+									<li class="nav__select-sub"><a href="#">Продукты питания<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+									<li class="nav__select-sub"><a href="#">Инструменты<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+									<li class="nav__select-sub"><a href="#">Быт. химия<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+								</ul> 
+							</li> 
+
+							<li class="nav__select"><a href="#" class="nav__select_blue">Школьная форма для девочек<div class="nav__select-icon">
+				  			<img src="img/header/arrow-list.png" alt="стрелка">
+				  		</div></a>
+				  				<ul>
+
+									<li class="nav__select-sub"><a href="#">Одежда<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+										<ul>
+
+											<li class="nav__select-sub2"><a href="#">Обувь<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Куртки<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Брюки<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+										</ul>
+									</li>
+
+									<li class="nav__select-sub"><a href="#">Электроника<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+										<ul>
+											<li class="nav__select-sub2"><a href="#">Камеры<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Компьютеры<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+											<li class="nav__select-sub2"><a href="#">Телефоны<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a>
+												<ul>
+
+													<li><a href="#">Samsung</a></li>
+													<li><a href="#">Flf</a></li>
+													<li><a href="#">Apple</a></li>
+												</ul>
+											</li>
+										</ul> 
+									</li> 
+
+									<li class="nav__select-sub"><a href="#">Продукты питания<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+									<li class="nav__select-sub"><a href="#">Инструменты<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+									<li class="nav__select-sub"><a href="#">Быт. химия<div class="nav__select-arrow">
+						  				<img src="img/header/arrow-list-right.png" alt="стрелка">
+						  			</div></a></li>
+								</ul> 
+							</li>
+				  	</ul> 
+		  	  </div>
+
+		  	  	<div class="nav__input-wrap">
+					  	<input type="text" class="nav__input-search" placeholder="Что Вы хотите найти?" maxlength="300">
+					  	<div class="nav__search-icon">
+					  		<img src="img/header/search-icon.jpg" alt="лупа">
+					  	</div>
+				  	</div>
+			 	</div>  
+			</div>  
+		</nav>
 	</header>
