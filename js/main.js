385b41754b803e1ead48f42010735bd3fa72953a
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-21 19:00:23 +0400

    переработал слайд

--- js/main.js
+++ js/main.js
@@ -11,42 +11,65 @@ arrowupEl.click(function() {
 
 
 // == слайдер ==
-$('.thing__slide').slick({
-	slidesToShow: 6,
-	slidesToScroll: 2,
-	infinite: true,
-	// autoplay: true,
-	prevArrow: $('.slide__left-arrow'),
-	nextArrow: $('.slide__rigth-arrow'),
-	 responsive: [
-  {
-    breakpoint: 1201,
-    settings: {
-      slidesToShow: 6,
-      slidesToScroll: 2,
-      infinite: true,
-    }
-  },
-  {
-    breakpoint: 1000,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  },
-  {
-    breakpoint: 320,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-  }
-  },
-]
+// $('.thing__slide').slick({
+// 	slidesToShow: 6,
+// 	slidesToScroll: 2,
+// 	infinite: true,
+// 	// autoplay: true,
+// 	prevArrow: $('.slide__left-arrow'),
+// 	nextArrow: $('.slide__rigth-arrow'),
+// 	 responsive: [
+//   {
+//     breakpoint: 1201,
+//     settings: {
+//       slidesToShow: 6,
+//       slidesToScroll: 2,
+//       infinite: true,
+//     }
+//   },
+//   {
+//     breakpoint: 1000,
+//     settings: {
+//       slidesToShow: 1,
+//       slidesToScroll: 1
+//     }
+//   },
+//   {
+//     breakpoint: 320,
+//     settings: {
+//       slidesToShow: 1,
+//       slidesToScroll: 1
+//   }
+//   },
+// ]
+
+
+
+// == двойной слайдер ==
+
+$('.thing__slide-main').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  asNavFor: '.thing__slide',
+  fade: true,
 
 });
 
+$('.thing__slide').slick({
+  slidesToShow: 6,
+  infinite: true,
+  asNavFor: '.thing__slide-main',
+  prevArrow: $('.slide__left-arrow'),
+  nextArrow: $('.slide__rigth-arrow'),
+  focusOnSelect: true,
+  centerPadding: '50px'
 
 
+
+
+});
 
 
 
